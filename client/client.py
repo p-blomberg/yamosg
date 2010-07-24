0c4bd18b29454d0fc39b5993dda3267934e6b8ba
David Sveningsson <ext@sidvind.com>
2010-07-24 13:46:09 +0000

    opengl window

--- client/client.py
+++ client/client.py
@@ -15,26 +15,81 @@ import socket, traceback
 from select import select
 from common import command
 
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
 def expose(func):
 	""" Exposes a method, eg is callable by server """
 	func.exposed = True
 	return func
 
+def setup_opengl():
+	glClearColor(1,0,1,0)
+	
 class Client:
-	def __init__(self, host='localhost', port=1234, split="\n"):
+	def __init__(self, resolution=(800,600), host='localhost', port=1234, split="\n"):
 		self._split = split
 		self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self._s.connect((host, port))
 		self._running = False
 		
+		self._screen = pygame.display.set_mode(resolution, OPENGL|DOUBLEBUF|RESIZABLE)
+		pygame.display.set_caption('yamosg')
+		
+		setup_opengl()
+	
+	def quit(self):
+		self._running = False
+	
 	def run(self):
 		self._running = True
 		while self._running:
-			(rlist, wlist, xlist) = select([self._s], [], [], 0.0)
+			self._network()
+			self._logic()
+			self._render()
+	
+	def _resize(self, width, height):
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		glOrtho(0, width, 0, height, -1.0, 1.0);
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
+	
+	def _network(self):
+		(rlist, wlist, xlist) = select([self._s], [], [], 0.0)
 			
-			if len(rlist) > 0:
-				cmd, args = command.parse(self._s.recv(8192))
-				self._dispatch(cmd, args)
+		if len(rlist) > 0:
+			cmd, args = command.parse(self._s.recv(8192))
+			self._dispatch(cmd, args)
+	
+	def _logic(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.quit()
+			elif event.type == pygame.VIDEOEXPOSE:
+				pass
+			elif event.type == pygame.VIDEORESIZE:
+				self._resize(event.width, event.height)
+			elif event.type == pygame.ACTIVEEVENT:
+				pass
+			elif event.type == pygame.MOUSEMOTION:
+				pass
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				pass
+			elif event.type == pygame.MOUSEBUTTONUP:
+				pass
+			elif event.type == pygame.KEYDOWN:
+				pass
+			elif event.type == pygame.KEYUP:
+				pass
+			else:
+				print 'Unhandled pygame event', event
+	
+	def _render(self):
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		pygame.display.flip()
 	
 	def _dispatch(self, cmd, args):
 		""" Run command """
@@ -60,5 +115,7 @@ class Client:
 		print 'got hello'
 
 if __name__ == '__main__':
+	pygame.display.init()
+	
 	client = Client()
 	client.run()
