755d6e132c31d2e884269bb8f19e10e8f69e7078
David Sveningsson <ext@sidvind.com>
2010-08-23 16:45:57 +0000

    storing size and position requests

--- client/ui/hbox.py
+++ client/ui/hbox.py
@@ -5,23 +5,25 @@ from OpenGL.GL import *
 
 class HBox:
       def __init__(self, *widgets):
-      	  self._widgets = widgets
+            self._widgets = []
+            for c in widgets:
+                  self.add(c)
 
       def is_invalidated(self):
             return any([x.is_invalidated() for x in self.get_children()])
 
       def get_children(self):
-            return self._widgets
+            return [widget for (widget, _, _) in self._widgets]
       
       def on_resize(self, size):
             dx = float(size.x) / len(self._widgets)
-            for n,c in enumerate(self._widgets):
-                  c.pos.y = 00
-                  c.pos.x = n*dx
-                  c.pos.z = 0.0
-                  c.size.x = dx
-                  c.size.y = size.y
-                  c.on_resize(c.size)
+            for n, (widget, wsize, wposition) in enumerate(self._widgets):
+                  widget.pos.y = 00
+                  widget.pos.x = n*dx
+                  widget.pos.z = 0.0
+                  widget.size.x = dx
+                  widget.size.y = size.y
+                  widget.on_resize(widget.size)
 
       def render(self):
             for x in self.get_children():
@@ -30,5 +32,8 @@ class HBox:
                   glPopMatrix()
 
       def display(self):
-            for c in self._widgets:
+            for c in self.get_children():
                   c.display()
+
+      def add(self, widget, size=None, position=None):
+            self._widgets.append((widget, size, position))
