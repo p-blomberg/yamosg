feb6597b777835e0f12bb50247d9e5de04830d6e
David Sveningsson <ext@sidvind.com>
2010-08-23 16:45:25 +0000

    adding layout object (to request size or position from a container)

---
+++ client/ui/layout.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class LayoutAttachment:
+    def __init__(self, relative, absolute):
+        """"
+        Layout attachment request, the container may or may not conform to the request.
+
+        :param relative: Percentage of container.
+        :param absolute: Pixel offset.
+        """
+
+        self.relative = relative
+        self.absolute = absolute
+
+
+        
