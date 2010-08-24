d8597a93f60295825ebc198ee6be5e3bc16ad182
David Sveningsson <ext@sidvind.com>
2010-08-24 15:33:13 +0000

    refactoring a bit to make it easier to reuse for the vbox

--- client/ui/hbox.py
+++ client/ui/hbox.py
@@ -2,21 +2,62 @@
 # -*- coding: utf-8 -*-
 
 from OpenGL.GL import *
+import functools
 
-class HBox:
+class _Box:
       def __init__(self, *widgets):
             self._widgets = []
             for c in widgets:
                   self.add(c)
+
+      def _default_size(self, size, field):
+            # get the sum of the requested widths
+            n = 0
+            sum = 0.0
+            for _, wsize, _ in self._widgets:
+                  if wsize is None:
+                        n += 1
+                        continue
+
+                  abs = wsize.get_absolute(size)
+                  sum += getattr(abs, field)
+
+            left = float(size.x - sum)
+
+            if left <= 0 or n == 0:
+                  return 0
+            
+            return left / n
 
       def is_invalidated(self):
             return any([x.is_invalidated() for x in self.get_children()])
 
       def get_children(self):
             return [widget for (widget, _, _) in self._widgets]
-      
+
+      def render(self):
+            for x in self.get_children():
+                  glPushMatrix()
+                  x.render()
+                  glPopMatrix()
+
+      def display(self):
+            for c in self.get_children():
+                  glPushMatrix()
+                  c.display()
+                  glPopMatrix()
+
+      def add(self, widget, size=None, position=None):
+            self._widgets.append((widget, size, position))
+
+class HBox(_Box):
+      _default_size = functools.partial(_Box._default_size, field='x')
+
+      def __init__(self, *widgets):
+            _Box.__init__(self, *widgets)
+
       def on_resize(self, size):
-            dx = self._default_width(size)
+            dx = self._default_size(self, size)
             x = 0.0
             for n, (widget, wsize, wposition) in enumerate(self._widgets):
                   widget.pos.y = 00
@@ -33,36 +74,3 @@ class HBox:
                         widget.size = abs
 
                   widget.on_resize(widget.size)
-
-      def _default_width(self, size):
-            # get the sum of the requested widths
-            n = 0
-            sum = 0.0
-            for _, wsize, _ in self._widgets:
-                  if wsize is None:
-                        n += 1
-                        continue
-
-                  sum += wsize.get_absolute(size).x
-
-            left = float(size.x - sum)
-
-            if left <= 0 or n == 0:
-                  return 0
-            
-            return left / n
-
-      def render(self):
-            for x in self.get_children():
-                  glPushMatrix()
-                  x.render()
-                  glPopMatrix()
-
-      def display(self):
-            for c in self.get_children():
-                  glPushMatrix()
-                  c.display()
-                  glPopMatrix()
-
-      def add(self, widget, size=None, position=None):
-            self._widgets.append((widget, size, position))
