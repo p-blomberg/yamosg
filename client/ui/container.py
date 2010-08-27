ebebd740383ff84018390bf6afdb4990a47d0405
David Sveningsson <ext@sidvind.com>
2010-08-27 11:27:01 +0000

    adding the new container base class

--- client/ui/container.py
+++ client/ui/container.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from ui import FBOWidget
+from ui import Widget, FBOWidget
 from window import BaseWindow
 
 from OpenGL.GL import *
 from copy import copy
+import inspect
 
-class Composite(FBOWidget):
-	def __init__(self, position, size, children=[], format=GL_RGB8, *args, **kwargs):
-		FBOWidget.__init__(self, position, size, *args, format=format, **kwargs)
+class Container:
+	def __init__(self, children=[], *args, **kwargs):
+		assert isinstance(self, Widget)
+
 		self._children = []
-
 		for i, child in enumerate(children):
 			self.add(child, zorder=i)
 		
 		self.sort()
-	
+
+	def add(self, widget, zorder=0):
+		widget.parent = self
+		widget.__zorder = zorder
+
+		self._children.append(widget)
+		self.invalidate()
+		self.sort()
+
+	def remove(self, widget):
+		widget.parent = None
+		self._children.remove(widget)
+		self.invalidate()
+
 	def get_children(self):
 		return self._children
-	
-	def hit_test(self, point, project=True):
-		if self._focus_lock:
-			return self._focus_lock, self._focus_lock.project(point)
-	
-		if project:
-			point = self.project(point)
-		
-		for x in reversed(self._children):
-			hit = x.hit_test(point, True)
-			if hit:
-				return hit
-		
-		return FBOWidget.hit_test(self, point, False)
 
 	def bring_to_front(self, widget):
 		for c in self._children:
@@ -46,26 +46,36 @@ class Composite(FBOWidget):
 		# renumber
 		n = len(self._children)
 		for i,c in enumerate(self._children):
-			c.__zorder = n - 1		
+			c.__zorder = n - 1
 
-	def add(self, widget, zorder=0):
-		widget.parent = self
-		widget.__zorder = zorder
-		self._children.append(widget)
-		self.invalidate()
-		self.sort()
+	def is_invalidated(self):
+		return any([x.is_invalidated() for x in self.get_children()])
+
+	def hit_test(self, point, project=True):
+		if self._focus_lock:
+			return self._focus_lock, self._focus_lock.project(point)
 	
-	def remove(self, widget):
-		widget.parent = None
-		self._children.remove(widget)
-		self.invalidate()
+		if project:
+			point = self.project(point)
+		
+		for x in reversed(self._children):
+			hit = x.hit_test(point, True)
+			if hit:
+				return hit
+		
+		return Widget.hit_test(self, point, False)
+
+class Composite(Container, FBOWidget):
+	def __init__(self, position, size, children=[], format=GL_RGB8, *args, **kwargs):
+		Container.__init__(self, children=children)
+		FBOWidget.__init__(self, position, size, *args, format=format, **kwargs)
 
 	def find_window(self, name):
 		# to make sure we dont find a window with id None
 		if name is None:
 			return None
 
-		for c in self._children:
+		for c in self.get_children():
 			if not isinstance(c, BaseWindow):
 				continue
 
@@ -77,7 +87,7 @@ class Composite(FBOWidget):
 		glClearColor(0,0.5,1,1)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 		
-		for x in self._children:
+		for x in self.get_children():
 			glPushMatrix()
 			x.display()
 			glPopMatrix()
